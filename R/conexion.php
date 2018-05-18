57cd945e45bf5e8ad607a0300b53444d5f06258d
AlexisJavierAstorga <alexisastorga72@gmail.com>
2018-05-17 20:55:58 -0500

    Con

--- R/conexion.php
+++ R/conexion.php
@@ -1,10 +1,15 @@
 <?php
 // Conectando, seleccionando la base de datos
-$conexion = new mysqli('localhost', 'id5772542_root', '123456', 'id5772542_prueba');
+$s="localhost";
+$bd="id5772542_prueba";
+$u="id5772542_root";
+$p="123456";
 
-if($conexion){
-  echo 'Conexión exitosa';
-}else{
+$conexion = new mysqli($s,$u,$p,$bd);
+
+if($conexion-> connect_errno){
   echo 'No se pudo conectar a la base de datos';
+}else{
+  echo 'Conexión exitosa';
 }
 ?>
