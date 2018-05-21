cc2c75c637a73dbc52c5c970c61e63dfe8750af9
AlexisJavierAstorga <alexisastorga72@gmail.com>
2018-05-21 03:12:08 -0500

    Conexion
    Funciona

--- R/conexion.php
+++ R/conexion.php
@@ -1,15 +1,4 @@
 <?php
 // Conectando, seleccionando la base de datos
-$s="localhost";
-$bd="id5772542_prueba";
-$u="id5772542_root";
-$p="123456";
-
-$conexion = new mysqli($s,$u,$p,$bd);
-
-if($conexion-> connect_errno){
-  echo 'No se pudo conectar a la base de datos';
-}else{
-  echo 'Conexión exitosa';
-}
+  $con=mysqli_connect('localhost','root','','rs') or die ("Error al conectar con la base de datos");
 ?>
