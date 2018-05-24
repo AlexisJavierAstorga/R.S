3d9fc192c9364e9749264ade88d65419978d359a
AlexisJavierAstorga <alexisastorga72@gmail.com>
2018-05-24 01:09:57 -0500

    Nueva
    Nueva conexion

--- R/conexion.php
+++ R/conexion.php
@@ -1,4 +1,9 @@
 <?php
 // Conectando, seleccionando la base de datos
-  $con=mysqli_connect('localhost','root','','rs') or die ("Error al conectar con la base de datos");
+  //$con=mysqli_connect('localhost','root','','rs') or die ("Error al conectar con la base de datos");
+$con = new mysqli("localhost", "root", "", "rs");
+if ($con->connect_errno) {
+    echo "Fallo al conectar a MySQL: (" . $con->connect_errno . ") " . $con->connect_error;
+}
+echo $con->host_info . "\n";
 ?>
