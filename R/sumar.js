e6471e64d4bff0083a3cb3927db05322efb577f8
AlexisJavierAstorga <alexisastorga72@gmail.com>
2018-06-01 18:28:30 -0500

    Final

--- R/sumar.js
+++ R/sumar.js
@@ -1,9 +1,11 @@
 function suma(){
-
          var numero1 = Number (document.getElementById ('txtNumero1').value);
          var numero2 = Number (document.getElementById ('txtNumero2').value);
          var totalsuma = numero1 * numero2;
          document.getElementById('totalsuma').value=totalsuma;
+
+         //Tecla de retroceso para borrar, siempre la permite
               }
+
 
              //suma();
