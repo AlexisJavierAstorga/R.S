866524412c343d8b8c1e592f8f701571aa084055
AlexisJavierAstorga <alexisastorga72@gmai.com>
2018-06-04 15:36:38 -0500

    Definitivo
    Compras

--- R/compraslog.php
+++ R/compraslog.php
@@ -23,6 +23,7 @@ if(isset($_SESSION['u_usuario'])){
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <link rel="shortcut icon" type="image/x-icon" href="themes/theme1362/img/icon/logo/LOGO.ico" />
 
+<link rel="stylesheet" href="themes/theme1362/css/tabla.css" media="all" />
 <link rel="stylesheet" href="themes/theme1362/css/inputs.css" media="all" />
               <link rel="stylesheet" href="themes/theme1362/css/global.css" media="all" />
               <link rel="stylesheet" href="themes/theme1362/css/video.css" media="all" />
@@ -313,7 +314,7 @@ include("conexion.php");
 
 //$result = mysqli_query($con, "SELECT (tbl_clientes.correo,tbl_inventarioVentas.IDPROD,tbl_inventarioVentas.nombre,tbl_inventarioVentas.cantidadProducto,tbl_inventarioVentas.totalVenta,tbl_inventarioVentas.fechaCompra) FROM tbl_clientes,tbl_inventarioProductos,tbl_inventarioVentas WHERE tbl_clientes.IDCLI=tbl_inventarioVentas.Cliente");
 
-$result = mysqli_query($con, "SELECT tbl_inventarioVentas.IDVEN,tbl_clientes.correo,tbl_inventarioProductos.IDPROD,Producto,cantidadProducto,totalVenta,fechaCompra FROM tbl_inventarioVentas,tbl_inventarioProductos,tbl_clientes WHERE tbl_inventarioVentas.Cliente=tbl_clientes.IDCLI AND tbl_inventarioProductos.IDPROD=tbl_inventarioVentas.Producto");
+$result = mysqli_query($con, "SELECT tbl_inventarioVentas.IDVEN,tbl_clientes.correo,tbl_inventarioProductos.IDPROD,Producto,cantidadProducto,totalVenta,fechaCompra FROM tbl_inventarioVentas,tbl_inventarioProductos,tbl_clientes WHERE tbl_inventarioVentas.Cliente=tbl_clientes.IDCLI='".$_SESSION['u_usuario']."' AND tbl_inventarioProductos.IDPROD=tbl_inventarioVentas.Producto");
 
 if ($row = mysqli_fetch_array($result)){
   echo "<table border = '1'><tr><td>ID Venta</td><td>Correo</td><td>ID Producto</td><td>Nombre producto</td><td>Cantidad del producto</td><td>Total venta</td><td>Fecha de compra</td></tr>";
