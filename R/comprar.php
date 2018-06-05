e8956dd11da4a9b11dc4ff7bb38cc24c34a69e24
AlexisJavierAstorga <alexisastorga72@gmai.com>
2018-06-05 13:38:36 -0500

    Cambio
    Cambio completo talla

--- R/comprar.php
+++ R/comprar.php
@@ -12,9 +12,10 @@ $numin = $_POST["numin"];
 $postal = $_POST["postal"];
 $txtNumero1 = $_POST["txtNumero1"];
 $txtNumero3 = $_POST["totalsuma"];
+$talla = $_POST["talla"];
 $fecha = $_POST["fecha"];
 
-$insertar = "INSERT INTO tbl_inventarioVentas (Cliente,Producto,estado,delegacion,colonia,calle,numeroExterior,numeroInterior,codigoPostal,cantidadProducto,totalVenta,fechaCompra) VALUES ('".$_SESSION['id']."','$idProducto','$estado','$delegacion','$colonia','$calle','$numex','$numin','$postal','$txtNumero1','$txtNumero3','$fecha')";
+$insertar = "INSERT INTO tbl_inventarioVentas (Cliente,Producto,talla,estado,delegacion,colonia,calle,numeroExterior,numeroInterior,codigoPostal,cantidadProducto,totalVenta,fechaCompra) VALUES ('".$_SESSION['id']."','$idProducto','$talla','$estado','$delegacion','$colonia','$calle','$numex','$numin','$postal','$txtNumero1','$txtNumero3','$fecha')";
 
 $resultado = mysqli_query($con, $insertar);
 if (!$resultado) {
--- R/compraslog.php
+++ R/compraslog.php
@@ -314,12 +314,12 @@ include("conexion.php");
 
 //$result = mysqli_query($con, "SELECT (tbl_clientes.correo,tbl_inventarioVentas.IDPROD,tbl_inventarioVentas.nombre,tbl_inventarioVentas.cantidadProducto,tbl_inventarioVentas.totalVenta,tbl_inventarioVentas.fechaCompra) FROM tbl_clientes,tbl_inventarioProductos,tbl_inventarioVentas WHERE tbl_clientes.IDCLI=tbl_inventarioVentas.Cliente");
 
-$result = mysqli_query($con, "SELECT tbl_inventarioVentas.IDVEN,tbl_clientes.correo,tbl_inventarioProductos.IDPROD,Producto,cantidadProducto,totalVenta,fechaCompra FROM tbl_inventarioVentas,tbl_inventarioProductos,tbl_clientes WHERE tbl_inventarioVentas.Cliente=tbl_clientes.IDCLI='".$_SESSION['u_usuario']."' AND tbl_inventarioProductos.IDPROD=tbl_inventarioVentas.Producto");
+$result = mysqli_query($con, "SELECT tbl_inventarioVentas.IDVEN,tbl_clientes.correo,tbl_inventarioProductos.IDPROD,tbl_inventarioProductos.nombre,cantidadProducto,totalVenta,fechaCompra FROM tbl_inventarioVentas,tbl_inventarioProductos,tbl_clientes WHERE tbl_inventarioVentas.Cliente=tbl_clientes.IDCLI AND tbl_inventarioProductos.IDPROD=tbl_inventarioVentas.Producto AND tbl_inventarioVentas.Cliente='".$_SESSION['id']."'");
 
 if ($row = mysqli_fetch_array($result)){
   echo "<table border = '1'><tr><td>ID Venta</td><td>Correo</td><td>ID Producto</td><td>Nombre producto</td><td>Cantidad del producto</td><td>Total venta</td><td>Fecha de compra</td></tr>";
   do {
-      echo "<tr><td>".$row["IDVEN"]."</td><td>".$row["correo"]."</td><td>".$row["IDPROD"]."</td><td>".$row["Producto"]."</td><td>".$row["cantidadProducto"]."</td><td>".$row["totalVenta"]."</td><td>".$row["fechaCompra"]."</td></tr> \n";
+      echo "<tr><td>".$row["IDVEN"]."</td><td>".$row["correo"]."</td><td>".$row["IDPROD"]."</td><td>".$row["nombre"]."</td><td>".$row["cantidadProducto"]."</td><td>".$row["totalVenta"]."</td><td>".$row["fechaCompra"]."</td></tr> \n";
     }while ($row = mysqli_fetch_array($result));
    echo "</table> \n";
 }else {
